8164236e8e0fccd8a3631d9199dd0d29e097590d
¨Gustavo <gustavo-machado-fontenele@hotmail.com>
2024-10-16 22:30:23 -0300

    feat: adjust eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,24 @@
 import pluginVue from 'eslint-plugin-vue';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
 export default [
-  // add more generic rulesets here, such as:
-  // js.configs.recommended,
-  // "plugin:vue/vue3-essential",
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
-  // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
-  // 'eslint:recommended',
-  // 'plugin:vue/vue3-recommended',
   {
-    files: ["**/*.ts", "**/*.vue"],
+    files: ["src/**/*.ts", "src/**/*.vue"],
+    plugins: {
+      'typescript-eslint': tseslint.plugin,
+    },
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        project: './tsconfig.app.json',
+        extraFileExtensions: ['.vue'],
+        sourceType: 'module',
+      },
+    },
     rules: {
       // override/add rules settings here, such as:
       'vue/no-unused-vars': 'error',
